97785337fe7c45fbe3c90d599d682fa467aaec2d
quirp <joesters@msn.com>
2023-11-16 18:17:31 -0800

    planning phase

---
+++ contracts/registry.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.9;
+
+contract FreezableRegistry{
+
+    struct Freezeable1{
+        parameter 
+    }
+    function freezableInit() external {
+        //should set merkle root for configurations
+        //should set initial state
+    }
+    function upgradeFreezable() external {
+
+    }
+    function getFreezables() external view returns  (bytes4[]){
+        return
+    }
+
+}
