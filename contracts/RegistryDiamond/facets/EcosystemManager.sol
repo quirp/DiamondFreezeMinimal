155fbcce4b71247e7d2b5509d4a7857928e03dac
quirp <joesters@msn.com>
2023-11-30 19:38:33 -0800

    uploadVersion method completed

--- contracts/RegistryDiamond/facets/EcosystemManager.sol
+++ contracts/RegistryDiamond/facets/EcosystemManager.sol
@@ -3,10 +3,15 @@ pragma solidity ^0.8.9;
 contract EcosystemManager{
     struct Ecosystem{
         bool isDisabled;
+        address ecosystemAddress;
         uint256 version; 
-        
+        //address is the key to parameters
     }
     struct EcosystemManage{
-
+        uint8 primaryEcosystemIndex;
+        Ecosystem[] ecosystems;
     }
+    function disableEcosystem(uint8 ecosystemIndex)external{}
+    function removeEcosystem(uint8 ecosystemIndex)external {}
+    function 
 }
--- contracts/RegistryDiamond/facets/RegistryManager.sol
+++ contracts/RegistryDiamond/facets/RegistryManager.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.9;
 Handles operations that are independent of version specifics
 Really need this to be the VersionManager(facet manager)
 Agnostic to implementation details, just adding new versions
+Should be responsible for router storage lookup and handling uploads
  */
 contract RegistryManager{
     function uploadVersion(uint256 version) external {
+        //let RegistryDiamond manage
         
     }
-
 }
